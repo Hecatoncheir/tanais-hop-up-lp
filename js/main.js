88f3a73dfbca69c1b10059f4ffe2285c7d10da1f
Rasarts <rasart.pro@gmail.com>
2014-06-30 16:03:27 +0300

    progressive enhancement added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$("#slide_to").fitText(1.3);
+$("#slide_to").fitText(1.3, {minFontSize: '14px'});
 $(".downloads a").fitText(1.115);
 $(".downloads a:last-child").fitText(1.215);
 $(".price_table tr").fitText(1.875);
