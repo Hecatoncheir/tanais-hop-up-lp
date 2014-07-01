920498dd8233c034a84339ff7ea44610a2079658
Rasarts <rasart.pro@gmail.com>
2014-07-01 12:34:41 +0300

    add tf for submit

--- js/main.js
+++ js/main.js
@@ -3,4 +3,4 @@ $(".downloads a").fitText(1.115);
 $(".downloads a:last-child").fitText(1.215);
 $(".price_table tr").fitText(1.875);
 $("article").fitText(1.875);
-#("#ftid").fitText(1);
+$("#ftid").fitText(1.37);
