81483e11840eafb004ef66c2b4222d326608e7b5
Rasarts <rasart.pro@gmail.com>
2014-07-01 15:35:39 +0300

    add fancybox for form

--- js/main.js
+++ js/main.js
@@ -4,6 +4,16 @@ $(".downloads a:last-child").fitText(1.215);
 $(".price_table tr").fitText(1.875);
 $("article").fitText(1.875);
 $("#ftid").fitText(1.37);
+$('#fb_open_div').fitText(2.37);
 
 
 $(".fb").fancybox();
+$(".fb_open").fancybox({
+    openEffect : 'fade',
+    padding: 0,
+    overflow: 'none'
+});
+//
+$('.close_fb_open_div').on('click', function(){
+    $('.fancybox-overlay').css('display', 'none');
+});
